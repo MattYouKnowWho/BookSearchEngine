7ed97e03c46b0819c6cbd4ef6d4c2bef17bfe086
Matt Dunston <matt.cdunston21@gmail.com>
2022-09-18 00:23:09 -0700

    fixed searchbook.js to work with apollo

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,10 +2,84 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
-import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { useMutation } from "@apollo/client";
+import { searchGoogleBooks } from "../utils/API";
+import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
+import { SAVE_BOOK } from "../utils/mutations";
+import { QUERY_ME } from "../utils/queries";
+
+const SearchBooks = () => {
+
+  const [searchedBooks, setSearchedBooks] = useState([]);
+
+  const [searchInput, setSearchInput] = useState("");
+
+  const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+  const [saveBook] = useMutation(SAVE_BOOK);
+
+  useEffect(() => {
+    return () => saveBookIds(savedBookIds);
+  });
+
+  
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!searchInput) {
+      return false;
+    }
+
+    try {
+      const response = await searchGoogleBooks(searchInput);
+
+      if (!response.ok) {
+        throw new Error(
+          `Something went wrong.. Unable to process your search request.`
+        );
+      }
+
+      const { items } = await response.json();
+
+      const bookData = items.map((book) => ({
+        bookId: book.id,
+        authors: book.volumeInfo.authors || ["No author to display"],
+        title: book.volumeInfo.title,
+        description: book.volumeInfo.description,
+        image: book.volumeInfo.imageLinks?.thumbnail || "",
+        link: book.volumeInfo.canonicalVolumeLink,
+      }));
+
+      setSearchedBooks(bookData);
+      setSearchInput("");
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
 
+  const handleSaveBook = async (bookId) => {
+   
+    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      await saveBook({
+        variables: { book: bookToSave },
+        refetchQueries: [{ query: QUERY_ME }],
+      });
+
+    
+      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <>

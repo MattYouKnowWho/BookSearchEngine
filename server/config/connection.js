a63669a7c7be2a3e394a55aae86ab04c936c6630
Matt Dunston <matt.cdunston21@gmail.com>
2022-09-17 22:45:15 -0700

    attempting to connect to mongodb cloud

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+const dotenv = require('dotenv').config()
+// console.log(process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,

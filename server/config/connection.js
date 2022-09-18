dea467bf9787eec11cedadec8cdc76c2ede0fb85
Matt Dunston <matt.cdunston21@gmail.com>
2022-09-17 22:47:09 -0700

    attempting to debug connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config()
+ require('dotenv').config()
 // console.log(process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
